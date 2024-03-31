5ae15d33057423ef574c7b941649a67c5c7c7490
Marlon0524 <marlonlopez401@gmail.com>
2024-03-31 16:49:10 -0500

    add dockerfile and loggers

--- src/main/java/com/employees/apirest/employee/EmployeeController.java
+++ src/main/java/com/employees/apirest/employee/EmployeeController.java
@@ -4,11 +4,13 @@ package com.employees.apirest.employee;
 import com.employees.apirest.department.Department;
 import com.employees.apirest.department.DepartmentRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/employee")
@@ -17,7 +19,8 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeService employeeService;
     private final DepartmentRepository departmentRepository;
-
+    // logger para ponerlo en el getallperson y resolver el punto Implementa un middleware para registrar logs de las llamadas que recibe el endpoint GET /employees
+    private static final Logger logger = Logger.getLogger(EmployeeController.class.getName());
     @PostMapping
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
         if(employee.getDepartment() == null || employee.getDepartment().getId() == null) {
@@ -35,6 +38,7 @@ public class EmployeeController {
     }
     @GetMapping
     public ResponseEntity<List<Employee>> getAllPersons() {
+        logger.info("Llamada al endpoint GET /employee recibida.");
         List<Employee> employees = employeeService.getAllEmployees();
         return ResponseEntity.ok(employees);
     }
